bd2a9e2c93eea56fe27cc4e428464518283bccdf
Eldyar <thedolbekov2@gmail.com>
2024-01-17 06:30:11 +0300

    added application form

---
+++ src/ApplicationForm/ApplicationForm.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import './form.css'
+import { data } from 'browserslist'
+
+
+const Form = () => {
+   const { register, handleSubmit } = useForm({})
+
+   const submit = data => {
+      console.log(data)
+   }
+
+   return (
+      <div className='application-form'>
+         <div className='application-form-content'>
+            <div className='application-form-content__desc'>
+               <div className='application-form-content__desc-title'>
+                  ПРИЕДЕМ, <br /> СНИМЕМ, <br /> ПОКАЖЕМ.
+               </div>
+               <p className='application-form-content__desc-subtitle'>
+                  Закажите производство трансляции, и в ближайшее время с вами свяжется наш менеджер для уточнения деталей
+               </p>
+            </div>
+            <form 
+               onSubmit={handleSubmit(submit)}
+               className='application-form-content__form' 
+               action="#"
+            >
+               <input {...register('activities')} placeholder='Мероприятие' type='text' />
+               <input {...register('contact')} placeholder='Контактное лицо' type='text' />
+               <input {...register('telephone')} placeholder='Телефон' type='tel' />
+               <input {...register('gmail')} placeholder='E-mail' type='text' />
+               <button className='application-form-content__form-btn'>Отправить заявку</button>
+            </form>
+         </div>
+      </div>
+   )
+}
+
+export default Form
--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -3,6 +3,7 @@ import Nav from "../Nav/Nav"
 import Footer from "../Footer/Footer"
 import { Outlet } from 'react-router-dom';
 import IconBfb from '../IconBfb/IconBfb';
+import Form from '../ApplicationForm/ApplicationForm'
 
 
 const Layout = () => {
@@ -11,6 +12,7 @@ const Layout = () => {
       <Nav/>
       <Outlet/>
       <IconBfb/>
+      <Form />
       <Footer/>
     </div>
   )
